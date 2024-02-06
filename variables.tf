0720974d1296fa322f22cb0ca810c3ebb1da9a8b
EC2 Default User <ec2-user@ip-172-31-52-100.us-west-2.compute.internal>
2024-02-06 18:58:40 +0000

    Adding variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "access_key" {
+  description = "The access key"
+  type        = string
+  sensitive   = true
+}
+variable "secret_key" {
+  description = "The secret key"
+  type        = string
+  sensitive   = true
+}
